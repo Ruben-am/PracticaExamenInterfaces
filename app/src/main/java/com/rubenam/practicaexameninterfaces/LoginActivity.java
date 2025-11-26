10dae8cb5e0ee2f28e043a085c962cccd524a258
Rubén <ru.alba.dev@gmail.com>
2025-11-26 20:46:12 +0100

    Feat/login activity (#2)
    * feat:add binding in LoginActivity
    
    * feat: add layout for loginActivity
    
    * feat: add custom buttons
    
    * feat: add gradient background
    
    * feat: add landscape view to login

---
+++ app/src/main/java/com/rubenam/practicaexameninterfaces/LoginActivity.java
@@ -0,0 +1,24 @@
+package com.rubenam.practicaexameninterfaces;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.rubenam.practicaexameninterfaces.databinding.ActivityLoginBinding;
+
+public class LoginActivity extends AppCompatActivity {
+
+    private ActivityLoginBinding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(binding.getRoot());
+        binding = ActivityLoginBinding.inflate(getLayoutInflater());
+    }
+}
