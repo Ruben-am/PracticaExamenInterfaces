83e1b8aabeb75ca78e78117e09747edf7949499f
Ruben-am <ru.alba.dev@gmail.com>
2025-11-26 19:48:53 +0100

    feat: add splash logic

--- app/src/main/java/com/rubenam/practicaexameninterfaces/SplashActivity.java
+++ app/src/main/java/com/rubenam/practicaexameninterfaces/SplashActivity.java
@@ -1,6 +1,8 @@
 package com.rubenam.practicaexameninterfaces;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,13 +10,35 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.rubenam.practicaexameninterfaces.databinding.ActivitySplashBinding;
+
 public class SplashActivity extends AppCompatActivity {
+
+    private ActivitySplashBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_splash);
 
+        binding = ActivitySplashBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
 
+        openMain();
+    }
+
+    private void openMain() {
+        Handler handler = new Handler(android.os.Looper.getMainLooper());
+        handler.postDelayed(
+                new Runnable() {
+                    @Override
+                    public void run() {
+                        Intent intent = new Intent(SplashActivity.this, MainActivity.class);
+                        intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK);
+                        startActivity(intent);
+                        finish();
+                    }
+                }, 5000
+        );
     }
 }
