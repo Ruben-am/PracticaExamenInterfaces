699574a6e41be28c305e85343e6810c2d8728c1a
Rubén <ru.alba.dev@gmail.com>
2025-11-26 20:02:12 +0100

    Feat/splash activity (#1)
    * feat: add splash activity elements and chage font style
    
    * feat: disable default app start
    
    * fix: glide implementation
    
    * feat: set splashActivity as start activity
    
    * feat: add splash logic
    
    * feat: add xml animation file
    
    * feat: add animation logic
    
    * feat: add background image (glide)

---
+++ app/src/main/java/com/rubenam/practicaexameninterfaces/SplashActivity.java
@@ -0,0 +1,58 @@
+package com.rubenam.practicaexameninterfaces;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.bumptech.glide.Glide;
+import com.rubenam.practicaexameninterfaces.databinding.ActivitySplashBinding;
+
+public class SplashActivity extends AppCompatActivity {
+
+    private ActivitySplashBinding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        // ImageView backImage = findViewById(R.id.iv_splash_backimage); sin binding
+
+        binding = ActivitySplashBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        Animation myAnim = AnimationUtils.loadAnimation(this, R.anim.rotation);
+        binding.ivSplashLogo.startAnimation(myAnim);
+
+        Glide.with(this)
+                .load("https://wallpapers.com/images/hd/dark-ocean-938-x-1668-wallpaper-9q07z71hrfl8k5ty.jpg")
+                .centerCrop()
+                .into(binding.ivSplashBackimage);
+
+        openMain();
+    }
+
+    private void openMain() {
+        Handler handler = new Handler(android.os.Looper.getMainLooper());
+        handler.postDelayed(
+                new Runnable() {
+                    @Override
+                    public void run() {
+                        Intent intent = new Intent(SplashActivity.this, MainActivity.class);
+                        intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK);
+                        startActivity(intent);
+                        finish();
+                    }
+                }, 5000
+        );
+    }
+}
