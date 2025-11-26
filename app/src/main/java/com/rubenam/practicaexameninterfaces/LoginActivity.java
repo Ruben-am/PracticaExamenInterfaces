e8abb101a7f82fc38d8b17022413081c22248095
Ruben-am <ru.alba.dev@gmail.com>
2025-11-26 23:04:28 +0100

    feat: sweetalert implemetation

--- app/src/main/java/com/rubenam/practicaexameninterfaces/LoginActivity.java
+++ app/src/main/java/com/rubenam/practicaexameninterfaces/LoginActivity.java
@@ -1,5 +1,6 @@
 package com.rubenam.practicaexameninterfaces;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.activity.EdgeToEdge;
@@ -10,6 +11,8 @@ import androidx.core.view.WindowInsetsCompat;
 
 import com.rubenam.practicaexameninterfaces.databinding.ActivityLoginBinding;
 
+import cn.pedant.SweetAlert.SweetAlertDialog;
+
 public class LoginActivity extends AppCompatActivity {
 
     private ActivityLoginBinding binding;
@@ -18,7 +21,31 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        setContentView(binding.getRoot());
         binding = ActivityLoginBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        binding.btnLoginSweet.setOnClickListener(view -> {
+            String usuario = binding.etLoginUser.getText().toString().trim();
+            String contrasenna = binding.etLoginPassword.getText().toString().trim();
+
+            if (usuario.isEmpty() || contrasenna.isEmpty()) {
+                new SweetAlertDialog(this, SweetAlertDialog.ERROR_TYPE)
+                        .setTitleText("Error")
+                        .setContentText("Introduce los datos para logearte")
+                        .show();
+            } else {
+                new SweetAlertDialog(this, SweetAlertDialog.SUCCESS_TYPE)
+                        .setTitleText("Hola " + usuario)
+                        .setContentText("Logeado correctamente")
+                        .setConfirmText("Continuar")
+                        .setConfirmClickListener(sweetAlertDialog -> {
+                            sweetAlertDialog.dismissWithAnimation();
+
+                            Intent intent = new Intent(this, SingUpActivity.class);
+                            startActivity(intent);
+                        })
+                        .show();
+            }
+        });
     }
 }
